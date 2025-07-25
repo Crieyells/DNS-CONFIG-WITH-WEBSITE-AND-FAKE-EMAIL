b7677de5019f6b5ac212a22859ae63f66530d7d2
RivanNate <166820244+Rivanyte@users.noreply.github.com>
2025-07-26 00:21:23 +0800

    Add files via upload

---
+++ bank-phishing-sites/CERTIFICATEWEBSITE.ps1
@@ -0,0 +1,25 @@
+﻿# Create self-signed certificate for www.bpi.ph.com
+$cert = New-SelfSignedCertificate -DnsName "www.bpi.ph.com" -CertStoreLocation "Cert:\LocalMachine\My"
+
+# Bind HTTPS to website with correct cert
+$siteName = "bpi.ph.com"
+$hostname = "www.bpi.ph.com"
+$certThumbprint = $cert.Thumbprint
+
+# Ensure SSL binding
+New-WebBinding -Name $siteName -Protocol https -Port 443 -HostHeader $hostname
+
+# Bind certificate to HTTPS binding
+Push-Location IIS:\SslBindings
+New-Item "0.0.0.0!443!$hostname" -Thumbprint $certThumbprint -SSLFlags 1
+Pop-Location
+
+# OPTIONAL: Import cert into Trusted Root if you want local trust automatically
+$certPath = "Cert:\LocalMachine\My\$certThumbprint"
+$rootStore = New-Object System.Security.Cryptography.X509Certificates.X509Store("Root","LocalMachine")
+$rootStore.Open("ReadWrite")
+$rootStore.Add((Get-Item $certPath))
+$rootStore.Close()
+
+# Restart IIS
+iisreset
